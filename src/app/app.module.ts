53811cd3448ee49a49bc6a233e441d787540fe85
vinopravin <vinopravin.m@gmail.com>
2020-03-28 12:51:51 +0530

    dummy components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './component-interaction/parent-to-child/component1/component1.component';
+import { Component2Component } from './component-interaction/parent-to-child/component2/component2.component';
+import { Component3Component } from './component-interaction/parent-to-child/component3/component3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component,
+    Component2Component,
+    Component3Component
   ],
   imports: [
     BrowserModule
